20594c454eb1badb01ae2e5907b83765c67c88e8
engin <engin@DESKTOP-BN63DI1>
2019-02-12 22:33:16 +0300

    qaz

--- src/a1.java
+++ src/a1.java
@@ -14,7 +14,7 @@ public class a1 {
 	public static void main0() {
 		// TODO Auto-generated method stub//5555555555552
 		//8522
-		//;;;;;;;;;;;;;;;;;;;;;;;;k;;;;;;;;;;;;
+		//;;;;;;;;;;;;;;;;;;;;;;;;;k;;;;;;;;;;;;
 		
 		
 		
