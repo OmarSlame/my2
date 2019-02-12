6d93af12aca7bc5f75a80ea3e441d0d11c260357
engin <engin@DESKTOP-BN63DI1>
2019-02-12 22:20:55 +0300

    sxsxs

--- src/a1.java
+++ src/a1.java
@@ -2,8 +2,9 @@
 public class a1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// TODO Auto-generated method stub//5555555555552
+		//8522
+		//;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 	}
 
 }
