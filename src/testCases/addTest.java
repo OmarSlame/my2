8047fea46aa69845dd637ef8bc429d5c4421239b
engin <engin@DESKTOP-BN63DI1>
2019-02-17 23:00:03 +0300

    kk

--- src/testCases/addTest.java
+++ src/testCases/addTest.java
@@ -1,6 +1,6 @@
 package testCases;
 
-import static org.junit.Assert.assertTrue;
+//import static org.junit.Assert.assertTrue;
 
 //import static org.junit.jupiter.api.Assertions.*;
 
@@ -10,7 +10,7 @@ class addTest {
 
 	@Test
 	void test() {
-assertTrue(true);
+//assertTrue(true);
 	}
 
 }
